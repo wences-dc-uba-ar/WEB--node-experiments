aa1bae594220b617dd2e34d380a194c82d0a60ce
wenceslao <wences@dc.uba.ar>
2016-11-02 23:41:46 -0300

    +bpmapi

---
+++ loopback/bpmapi/server/boot/create_sample_model.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = function(server) {
+	
+  // server.dataSources.bpm.automigrate('people', function(err) {
+  // 	if(err) {
+  //   	console.log(err.message);
+  // 	} else {
+  //   	console.log('no error messages');
+  // 	}
+  // });
+};
