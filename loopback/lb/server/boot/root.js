991c3aefc30ef58f1ab0fba1aa2cdbd7636b15b3
wenceslao <wences@dc.uba.ar>
2016-11-17 00:11:18 -0300

    +debug

--- loopback/lb/server/boot/root.js
+++ loopback/lb/server/boot/root.js
@@ -1,8 +1,12 @@
 'use strict';
 
+var log = require('debug')('boot:root!');
+
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
   server.use(router);
+
+  log('boot root done!');
 };
