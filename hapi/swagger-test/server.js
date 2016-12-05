55953d8e060176f437e17c780cf07004dae1012a
WAZ <wences@dc.uba.ar>
2016-12-05 18:13:20 -0300

    +hapi swagger

---
+++ hapi/swagger-test/server.js
@@ -0,0 +1,36 @@
+const Hapi = require('hapi');
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+
+const server = new Hapi.Server();
+server.connection({
+        host: 'localhost',
+        port: 3000
+    });
+
+const options = {
+    info: {
+            'title': 'Test API Documentation',
+            'version': Pack.version,
+        }
+    };
+
+server.register([
+    Inert,
+    Vision,
+    {
+        'register': HapiSwagger,
+        'options': options
+    }], (err) => {
+        server.start( (err) => {
+           if (err) {
+                console.log(err);
+            } else {
+                console.log('Server running at:', server.info.uri);
+            }
+        });
+    });
+
+server.route(Routes);
